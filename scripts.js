be92f16abfa5a746b1d5e5578505dd174b5a3a83
M Tanveer <mohammadtanveer7540@gmail.com>
2024-03-03 13:22:36 +0530

    added js code.

---
+++ scripts.js
@@ -0,0 +1,71 @@
+/*
+Winning Possibilities: 
+[
+    [1, 2, 3], 
+    [4, 5, 6],
+    [7, 8, 9], 
+    [1, 4, 7], 
+    [2, 5, 8,], 
+    [3, 6, 9], 
+    [1, 5, 9], 
+    [3, 5, 7]
+]
+
+
+*/
+
+let boxes = document.querySelectorAll(".box"); 
+let resetBtn = document.querySelector("#reset-btn"); 
+
+let turnO = true; 
+
+const winPatterns = [
+    [0, 1, 2], 
+    [3, 4, 5],
+    [6, 7, 8], 
+    [0, 3, 6], 
+    [1, 4, 7], 
+    [2, 5, 8], 
+    [0, 4, 8], 
+    [2, 4, 6]
+];
+
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        console.log("box was clicked.")
+        if(turnO){
+            box.innerText = "O"; 
+            turnO = false; 
+        }
+        else{
+            box.innerText = "X"; 
+            turnO = true; 
+        }
+        box.disabled = true;
+
+        checkWinner();
+    });
+    
+});
+
+const disableBoxes = () => {
+    for (let box of boxes){
+        box.disabled = true; 
+    }
+}
+const checkWinner = () => {
+    for (let pattern of winPatterns) {
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;  
+        let pos3Val = boxes[pattern[2]].innerText; 
+
+        if (pos1Val != "" && pos2Val != "" && pos3Val != ""){
+            if (pos1Val === pos2Val && pos2Val === pos3Val) {
+                // console.log(pos1Val + " is Winner!!"); 
+                let msg = document.querySelector("#msg"); 
+                msg.innerText = pos1Val + " is Winner!!"
+                disableBoxes(); 
+            }
+        }
+    }
+};
